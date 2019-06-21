b6358c05796756f2e1e307983bce455de09f4d68
Vladislav Pavluschik <plusch321@mail.ru>
2019-06-21 21:42:20 +0300

    init: project

---
+++ precache-manifest.c5e434765f944c1b8253440ad7c45f16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529383ec139bc86841a37deaca0436e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ab72aaa230a27fbcdf7",
+    "url": "/static/css/main.9e3bf354.chunk.css"
+  },
+  {
+    "revision": "8e5b09fd7e26e9b2822a",
+    "url": "/static/js/2.1286a3e1.chunk.js"
+  },
+  {
+    "revision": "4ab72aaa230a27fbcdf7",
+    "url": "/static/js/main.aa2718ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d76db389d8817b4a144ab39690d50a3c",
+    "url": "/static/media/app-screenshot.d76db389.png"
+  },
+  {
+    "revision": "110fa3f8f3510b27a88bd3e3f5d2541d",
+    "url": "/static/media/fontello.110fa3f8.eot"
+  },
+  {
+    "revision": "14a8ee41ab6101eb9b5b3f705df78219",
+    "url": "/static/media/fontello.14a8ee41.woff2"
+  },
+  {
+    "revision": "4ae788bcae8283dad55c21aa775f8ed7",
+    "url": "/static/media/fontello.4ae788bc.svg"
+  },
+  {
+    "revision": "9e1c7f233c4ce23a6d93ddfa86bd6801",
+    "url": "/static/media/fontello.9e1c7f23.ttf"
+  },
+  {
+    "revision": "a8068c0a76c9d27c82fd214cc946603f",
+    "url": "/static/media/fontello.a8068c0a.woff"
+  },
+  {
+    "revision": "1a49d9a107090d793c597367b8f5bef1",
+    "url": "/static/media/pix.1a49d9a1.ttf"
+  }
+]);
